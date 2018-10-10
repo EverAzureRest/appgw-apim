fd0fd6207252005e0ac83e63a6444bc9ed50c7b0
Jordan Smith <jorsmith@microsoft.com>
2018-10-10 12:26:03 -0700

    refactored, and remediated cert issues

--- build-testenv.ps1
+++ build-testenv.ps1
@@ -1,22 +1,23 @@
 ﻿param(
-    $subscriptionName = "",
-    $resourceGroupName = "",
-    $location = "",
+    $subscriptionName = "jorsmith-labsub1",
+    $resourceGroupName = "appgw-apimtest",
+    $location = "westus2",
     $appgwsubnetprefix = "10.0.0.0/24",
     $appgwsubnetName = "appgwsubnet",
     $apimsubnetprefix = "10.0.1.0/24",
     $apimsubnetName = "apimsubnet",
-    $vnetName = "",
+    $vnetName = "apim-vnet",
     $vnetPrefix = "10.0.0.0/16",
     $apimSKU = "Developer",
-    $apimServiceName = "",
-    $apimOrgName = "",
-    $apimAdminEmail = "",
-    $apimGatewayHostName = "",
-    $apimDevPortalHostName = "",
-    $certdir = "",
-    $apimGatewayCertPassword = "",
-    $apimDevPortalCertPassword = ""
+    $apimServiceName = "jorsmithapimservice",
+    $apimOrgName = "powerhell",
+    $apimAdminEmail = "jordan.smith@powerhell.org",
+    $apimGatewayHostName = "apim.cloud.powerhell.org",
+    $apimDevPortalHostName = "apim-dev.cloud.powerhell.org",
+    $certdir = "c:\users\jorsmith\temp\certs",
+    $apimGatewayCertPassword = "ThisisAWEAKPassword",
+    $apimDevPortalCertPassword = "THisisAlsoAweakPassword",
+    $appGatewayName = "apim-app-gw"
 )
 
 
@@ -91,9 +92,11 @@ catch {
 try {
     $apimdevportalcert = New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName $apimDevPortalHostName
     $apimDevPortalSecStringCertpw = $apimDevPortalCertPassword | ConvertTo-SecureString -AsPlainText -Force
-    $path = 'Cert:\CurrentUser\My\' + $apimdevportalcert.Thumbprint
+    $devportalcertpath = 'Cert:\CurrentUser\My\' + $apimdevportalcert.Thumbprint
     $apimDevPortalCertPfx = $certdir + '\apimdevportal.pfx'
-    Export-PfxCertificate -Cert $path -FilePath $apimDevPortalCertPfx -Password $apimDevPortalSecStringCertpw
+    $apimDevPortalCertCer = $certdir + '\apimdevportal.cer'
+    Export-PfxCertificate -Cert $devportalcertpath -FilePath $apimDevPortalCertPfx -Password $apimDevPortalSecStringCertpw
+    Export-Certificate -Cert $devportalcertpath -FilePath $apimDevPortalCertCer
 }
 catch {
     $exception = $_.Exception
@@ -103,10 +106,9 @@ catch {
 
 #Upload the Certificates
 try {
-    $apimgwsecStringcertpw = $apimGatewayCertPassword | ConvertTo-SecureString -AsPlainText -Force
-    $apimDevPortalSecStringCertpw = $apimDevPortalCertPassword | ConvertTo-SecureString -AsPlainText -Force
-    $gwCertUploadResult = Import-AzureRmApiManagementHostnameCertificate -ResourceGroupName $resourceGroupName -Name $apimServiceName -HostnameType "Proxy" -PfxPath $apimGatewayCertPfx -PfxPassword $apimgwsecStringcertpw -PassThru
-    $devPortalCertUploadResult = Import-AzureRmApiManagementHostnameCertificate -ResourceGroupName $resourceGroupName -Name $apimServiceName -HostnameType "Proxy" -PfxPath $apimDevPortalCertPfx -PfxPassword $apimDevPortalSecStringCertpw -PassThru
+
+    $gwCertUploadResult = Import-AzureRmApiManagementHostnameCertificate -ResourceGroupName $resourceGroupName -Name $apimServiceName -HostnameType "Proxy" -PfxPath $apimGatewayCertPfx -PfxPassword $apimGatewayCertPassword -PassThru
+    $devPortalCertUploadResult = Import-AzureRmApiManagementHostnameCertificate -ResourceGroupName $resourceGroupName -Name $apimServiceName -HostnameType "Proxy" -PfxPath $apimDevPortalCertPfx -PfxPassword $apimDevPortalCertPassword -PassThru
     Write-Debug -Message "Status of Gateway Cert upload is $($gwCertUploadResult)"
     Write-Debug -Message "Status of Dev Portal Cert upload is $($devportalcertuploadresult)"    
 }
@@ -147,3 +149,42 @@ catch {
     Write-Error $exception
     throw $exception
 }
+
+$apimgwprobe = New-AzureRmApplicationGatewayProbeConfig -Name "apimproxyprobe" -Protocol "Https" -HostName $apimGatewayHostName -Path "/status-0123456789abcdef" -Interval 30 -Timeout 120 -UnhealthyThreshold 8
+$apimdevPortalProbe = New-AzureRmApplicationGatewayProbeConfig -Name "apimportalprobe" -Protocol "Https" -HostName $apimDevPortalHostName -Path "/signin" -Interval 60 -Timeout 300 -UnhealthyThreshold 8
+
+
+$apimgwauthcert = New-AzureRmApplicationGatewayAuthenticationCertificate -Name "whitelistcert1" -CertificateFile $apimGatewayCertCer
+$apimdevportalauthcert = New-AzureRmApplicationGatewayAuthenticationCertificate -Name "DevPortalWhitelist" -CertificateFile $apimDevPortalCertCer
+
+$apimPoolSetting = New-AzureRmApplicationGatewayBackendHttpSettings -Name "apimPoolSetting" -Port 443 -Protocol "Https" -CookieBasedAffinity "Disabled" -Probe $apimgwprobe -AuthenticationCertificates $apimgwauthcert -RequestTimeout 180
+$apimPoolPortalSetting = New-AzureRmApplicationGatewayBackendHttpSettings -Name "apimPoolPortalSetting" -Port 443 -Protocol "Https" -CookieBasedAffinity "Disabled" -Probe $apimdevPortalProbe -AuthenticationCertificates $apimdevportalauthcert -RequestTimeout 180
+
+$apimProxyBackendPool = New-AzureRmApplicationGatewayBackendAddressPool -Name "apimbackend" -BackendIPAddresses $apimService.PrivateIPAddresses[0]
+
+$rule01 = New-AzureRmApplicationGatewayRequestRoutingRule -Name "rule1" -RuleType Basic -HttpListener $gwlistener -BackendAddressPool $apimProxyBackendPool -BackendHttpSettings $apimPoolSetting
+$rule02 = New-AzureRmApplicationGatewayRequestRoutingRule -Name "rule2" -RuleType Basic -HttpListener $devportalListener -BackendAddressPool $apimProxyBackendPool -BackendHttpSettings $apimPoolPortalSetting
+
+$sku = New-AzureRmApplicationGatewaySku -Name "WAF_Medium" -Tier "WAF" -Capacity 2
+$config = New-AzureRmApplicationGatewayWebApplicationFirewallConfiguration -Enabled $true -FirewallMode "Prevention"
+
+$appgwparams = @{
+    Name = $appGatewayName
+    ResourceGroupName = $resourceGroupName
+    Location = $location
+    BackendAddressPools = $apimProxyBackendPool
+    BackendHttpSettingsCollection = @($apimPoolSetting, $apimPoolPortalSetting)
+    FrontendIpConfiguration = $fipconfig01
+    GatewayIPConfigurations = $gipconfig
+    FrontEndPorts = $fePort
+    HttpListeners = @($gwlistener, $devportalListener)
+    RequestRoutingRules = @($rule01, $rule02)
+    Sku = $sku
+    WebApplicationFirewallConfig = $config
+    SslCertificates = @($apimgatewaycert, $apimdevportalcert)
+    AuthenticationCertificates = @($apimdevportalauthcert, $apimdevportalauthcert)
+    Probes = @($apimgwprobe, $apimdevPortalProbe)
+}
+
+
+$appgwObject = New-AzureRmApplicationGateway @appgwparams
